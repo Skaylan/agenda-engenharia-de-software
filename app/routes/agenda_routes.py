4a6d664b02927821aae8348ccf8e29b37cdd4add
Lucas Oliveira <lucas_7tv@hotmail.com>
2025-09-17 03:11:52 -0300

    Refactor agenda routes to use add_url_rule for consistency and clarity

--- app/routes/agenda_routes.py
+++ app/routes/agenda_routes.py
@@ -4,8 +4,31 @@ from app.controllers.agenda_controller import AgendaController
 
 agenda_bp = Blueprint('agenda_routes', __name__)
 
+agenda_bp.add_url_rule(
+    "/api/v1/adicionar_contato",
+    view_func=AgendaController.adicionar_contato,
+    methods=["POST"]
+)
 
-@agenda_bp.post('/api/v1/adicionar_contato')(AgendaController.adicionar_contato)
+agenda_bp.add_url_rule(
+    "/api/v1/localizar_contato",
+    view_func=AgendaController.localizar_contato,
+    methods=["GET"]
+)
+
+agenda_bp.add_url_rule(
+    "/api/v1/remover_contato",
+    view_func=AgendaController.remover_contato,
+    methods=["DELETE"]
+)
+
+agenda_bp.add_url_rule(
+    "/api/v1/listar_contatos",
+    view_func=AgendaController.listar_contatos,
+    methods=["GET"]
+)
+
+# @agenda_bp.post('/api/v1/adicionar_contato')(AgendaController.adicionar_contato)
 
 
 def register_routes(app):
